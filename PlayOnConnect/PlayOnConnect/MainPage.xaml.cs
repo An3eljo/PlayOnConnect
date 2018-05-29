72bf3fee034e27c65048ed190f6cff11f64c6a71
andrijoos <andri.joos@edu.tbz.ch>
2018-05-29 22:20:06 +0200

    aj:

---
+++ PlayOnConnect/PlayOnConnect/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PlayOnConnect
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+		    var user = E.Deezer.DeezerSession.CreateNew();
+            //user.Login().RunSynchronously();
+			InitializeComponent();
+		}
+
+	    private void WebView_OnNavigating(object sender, WebNavigatingEventArgs e)
+	    {
+	       if (e.Url.Contains("http://deezerauth.maneu.net"))
+	        {
+	            e.Cancel = true;
+	        }
+        }
+	}
+}
